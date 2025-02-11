1731826b788354735ade89dd4ac42c7aa9e66460
jeffcheasey1337 <90761999+jeffcheasey1337@users.noreply.github.com>
2025-02-12 00:17:47 +0300

    -added autoeat -added automood

---
+++ src/modules/OtherService/automood.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+# Загружаем эталонное изображение
+template = cv2.imread('../../../resources/images/ImgMood/Mood.png', 0)
+
+
+def find_template_on_screen(template, threshold=0.9):
+    """Ищет шаблон на экране, возвращает True, если найдено."""
+    screenshot = pyautogui.screenshot()
+    screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+    gray_screen = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.matchTemplate(gray_screen, template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
+
+    return len(loc[0]) > 0
+
+
+# Бесконечный цикл поиска
+while True:
+    if find_template_on_screen(template):
+        while find_template_on_screen(template):
+            pyautogui.press("l")  # Нажимает ;
+            print("Обнаружен недостаток настроения, нажата клавиша l")
+            time.sleep(10)
+
+    time.sleep(15)
